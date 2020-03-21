87f4efd6b6f9aed25b2d30c4c5926e448245fa84
mookthanatcha <mookkyammy@gmail.com>
2020-03-21 21:52:13 +0700

    เชื่อม backend กับ frontend

---
+++ API/ReadingAPI.js
@@ -0,0 +1,8 @@
+const reading = async (id) =>{
+    const response = await fetch(`http://10.0.2.2:3000/categorys`);
+    const data = await response.json() 
+    return data;
+}
+
+export default reading
+// const response = await fetch(`http://localhost:3000/categorys`);
--- Screens/InterestScreen.js
+++ Screens/InterestScreen.js
@@ -1,106 +1,147 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
   ScrollView,
-  ImageBackground
+  ImageBackground,
+  FlatList
 } from "react-native";
 import { Card, Button } from "react-native-elements";
 import { Grid, Col, Row } from "react-native-easy-grid";
 import { FlatGrid } from "react-native-super-grid";
+import ReadingApi from "../API/ReadingAPI"
+import { List, ListItem } from "react-native-elements"
 
-const items = [
-  {
-    name: "Music",
-    img: require("./../assets/catagory/music.png")
-  },
-  { name: "Movie", img: require("./../assets/catagory/movie.png") },
-  {
-    name: "News",
-    img: require("./../assets/catagory/news.png")
-  },
-  { name: "Animal", img: require("./../assets/catagory/animal.png") },
-  {
-    name: "Food",
-    img: require("./../assets/catagory/food.png")
-  },
-  { name: "Story", img: require("./../assets/catagory/story.png") },
-  {
-    name: "House",
-    img: require("./../assets/catagory/house.png")
-  },
-  {
-    name: "Natural",
-    img: require("./../assets/catagory/natural.png")
-  },
-  { name: "Sport", img: require("./../assets/catagory/sport.png") }
-];
+
 
 const ImageCards = () => {
   return <Image />;
 };
 
 const Interest = () => {
+  const [result, setResult] = useState([]);
+  const read = async () => {
+    const data = await ReadingApi();
+    setResult(data);
+    // console.log(data);
+    // console.log("-----------------")
+    // console.log(result)
+  };
+  console.log(result)
+  useEffect(() => {
+    read();
+  }, []);
+
   // const [count, setcount] = useState(0);
+  if (result) {
+    console.log("Eiei")
+    console.log(result)
+    console.log("Mookkkkkkkkk")
+    console.log(result.length)
 
-  return (
-    <ScrollView>
-      <Grid>
-        <Row>
-          <View style={styles.container}>
+    const items = [
+      {
+        name: "Music",
+        img: require("./../assets/catagory/music.png")
+      },
+      { name: "Movie", img: require("./../assets/catagory/movie.png") },
+      {
+        name: "News",
+        img: require("./../assets/catagory/news.png")
+      },
+      { name: "Animal", img: require("./../assets/catagory/animal.png") },
+      {
+        name: "Food",
+        img: require("./../assets/catagory/food.png")
+      },
+      { name: "Story", img: require("./../assets/catagory/story.png") },
+      {
+        name: "House",
+        img: require("./../assets/catagory/house.png")
+      },
+      {
+        name: "Natural",
+        img: require("./../assets/catagory/natural.png")
+      },
+      { name: "Sport", img: require("./../assets/catagory/sport.png") }
+    ];
+    return (
+      <ScrollView>
+        <Grid>
+          <Row>
             <View style={styles.container}>
-              <Text style={styles.topic}>Interest</Text>
-              <Text style={styles.descript}>
-                Please select 3 that you interest
+              <View style={styles.container}>
+                <Text style={styles.topic}>Interest</Text>
+                <Text style={styles.descript}>
+                  Please select 3 that you interest
               </Text>
+              </View>
             </View>
-          </View>
-        </Row>
-        <Row>
-          <View style={styles.container}></View>
-        </Row>
+          </Row>
+          <Row>
+            <View style={styles.container}></View>
+          </Row>
+          <Row>
+            <View style={{ flex: 1, paddingTop: 20 }}>
+              <FlatList
+                data={result}
+                renderItem={({ item }) => (
+                  <ListItem
+                  roundAvatar
+                  title={item.categoryName}
+                  subtitle={item.typeName}
 
-        <FlatGrid
-          itemDimension={110}
-          items={items}
-          style={styles.gridView}
-          renderItem={({ item, index }) => (
-            <Card
-              containerStyle={styles.itemContainer}
-              image={item.img}
-              imageStyle={{
-                height: "65%"
-                // borderColor: "red",
-                // borderTopLeftRadius: 5,
-                // borderTopRightRadius: 5,
-              }}
-              imageProps={{ resizeMode: "cover" }}
-            >
-              {/* <Image
+                />
+
+                )
+                }
+                keyExtractor={({ id }, index) => id}
+              />
+            </View>
+          </Row>
+
+
+          <FlatGrid
+            itemDimension={110}
+            items={items}
+            style={styles.gridView}
+            renderItem={({ item, index }) => (
+              <Card
+                containerStyle={styles.itemContainer}
+                image={item.img}
+                imageStyle={{
+                  height: "65%"
+                  // borderColor: "red",
+                  // borderTopLeftRadius: 5,
+                  // borderTopRightRadius: 5,
+                }}
+                imageProps={{ resizeMode: "cover" }}
+              >
+                {/* <Image
               source={item.img}
               style={{ height: "100%" }}
               resizeMode="cover"
             /> */}
-              <View style={{ alignItems: "center" }}>
-                <Text style={styles.itemTopic}>{item.name}</Text>
-              </View>
-            </Card>
-          )}
-        />
-        <Row style={styles.container}>
-          <Button title="Next" buttonStyle={styles.button} />
-        </Row>
-      </Grid>
-      {/* <View style={styles.container}>
+                <View style={{ alignItems: "center" }}>
+                  <Text style={styles.itemTopic}>{item.name}</Text>
+                </View>
+              </Card>
+            )}
+          />
+          <Row style={styles.container}>
+            <Button title="Next" buttonStyle={styles.button} />
+          </Row>
+        </Grid>
+        {/* <View style={styles.container}>
           <TouchableOpacity onPress={() => setcount(count + 1)}>
             <Image source={require("./../assets/icon.png")} />
           </TouchableOpacity>
           <Text>your clicks: {count}</Text>
           <Text>click</Text>
         </View> */}
-      {/* <FlatGrid
+        {/* <FlatGrid
           itemDimension={110}
           items={items}
           style={styles.gridView}
@@ -116,8 +157,12 @@ const Interest = () => {
             />
           )}
         /> */}
-    </ScrollView>
-  );
+      </ScrollView>
+    )
+  }
+  else {
+    return <Text>Loading</Text>
+  };
 };
 
 export default Interest;
