a5d4c013a333a5dbf919a586bf1eaef986e5e645
mookthanatcha <mookkyammy@gmail.com>
2020-03-25 00:35:25 +0700

    [UPDATE] prepare api

--- App/API/ReadingAPI.js
+++ App/API/ReadingAPI.js
@@ -1,8 +1,24 @@
-const reading = async (id) =>{
+const reading = async (id) => {
     const response = await fetch(`http://10.0.2.2:3000/categorys/reading`);
-    const data = await response.json() 
+    const data = await response.json()
     return data;
 }
+// const postView = async => {
+//     const response = axios({
+//         method: 'post',
+//         url: 'http://10.0.2.2:3000/views',
+//         data: {
+//             "views_id" : "2",
+//             "numOfView" : "2",
+//             "category_id" : "1",
+//             "user_id" : "1",
+//             "reading_id" : "1",
+//             "vocabBox_id" : "1"
+//         }
+//     });
+//     const data = await response.json()
+//     return data;
 
+// }
 export default reading
 // const response = await fetch(`http://localhost:3000/categorys`);
