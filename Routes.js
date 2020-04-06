06e0daea99162018156e2b1e9e3b2d89fd8285ce
Jenny <jnjiraphat@gmail.com>
2020-04-07 03:12:38 +0700

    [ADD] Routes: Home

--- Routes.js
+++ Routes.js
@@ -4,16 +4,24 @@ import Article from "./App/Screens/Article.js";
 import MaybeYouLike from "./App/Screens/MaybeYouLike";
 import InterestScreen from "./App/Screens/InterestScreen.js";
 import ContentScreen from "./App/Screens/ContentScreen";
+import Home from "./App/Screens/Home";
 // import Article from "./App/Screens/Article"
 
 const Routes = () => (
   <Router>
     <Scene key="root">
-      <Scene
-        key="home"
-        component={InterestScreen}
-        title="Homes"
+      {/* <Scene
+        key="Home"
+        component={Home}
+        title="Home"
         initial={true}
+        hideNavBar={true}
+      /> */}
+      <Scene
+        key="Interest"
+        component={InterestScreen}
+        title="Interest"
+        // initial={true}
         hideNavBar={true}
       />
       <Scene key="MaybeYouLike" component={MaybeYouLike} title="MaybeYouLike" />
