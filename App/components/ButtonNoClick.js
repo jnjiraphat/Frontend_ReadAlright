cd287abd45d6e90e989d355e9cd0f6b32a5786e9
Jenny <jnjiraphat@gmail.com>
2020-04-10 15:42:02 +0700

    [ADD] ButtonNoClick Component

---
+++ App/components/ButtonNoClick.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const ButtonClick = (props) => {
+  const {
+    colorsStart,
+    colorsEnd,
+    padding,
+    radius,
+    height,
+    width,
+    fontSize,
+    fontWeight,
+    fontcolor,
+    text,
+    buttonStyle,
+  } = props;
+  return (
+    <View style={{ justifyContent: "center" }}>
+      <LinearGradient
+        colors={[colorsStart, colorsEnd]}
+        style={{
+          padding: padding,
+          alignItems: "center",
+          borderRadius: radius,
+          height: height,
+          width: width,
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowRadius: 4.65,
+          shadowColor: "#000000",
+          shadowOpacity: 0.3,
+          elevation: 8,
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            color: fontcolor,
+          }}
+        >
+          {text}
+        </Text>
+      </LinearGradient>
+    </View>
+  );
+};
+
+export default ButtonClick;
