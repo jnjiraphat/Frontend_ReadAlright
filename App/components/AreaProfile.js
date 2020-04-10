3c1facac205891e33c49260bc47a2eb1ae9ce44e
Jenny <jnjiraphat@gmail.com>
2020-04-10 15:43:47 +0700

    [EDIT] Change Code > ButtonNoClick Component

--- App/components/AreaProfile.js
+++ App/components/AreaProfile.js
@@ -1,50 +1,34 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import ButtonNoClick from "../components/ButtonNoClick";
 
 const AreaProfile = (props) => {
   const [name, showName] = useState("Thanatcha");
-  const { level, display } = props;
+  const { display, level } = props;
   const [sugggestion, hideSuggestion] = useState(
     "You should practice adverb more"
   );
   return (
     <View style={{ flexDirection: "column" }}>
       <View style={{ flexDirection: "row" }}>
-        <LinearGradient
-          colors={["#86B8F3", "#2DC897"]}
-          style={{
-            marginRight: 15,
-            // position: "absolute",
-            // left: 167,
-            borderRadius: 5,
-            height: 26,
-            width: 51,
-            shadowOffset: {
-              width: 0,
-              height: 4,
-            },
-            shadowRadius: 4.65,
-            shadowColor: "#000000",
-            shadowOpacity: 0.3,
-            elevation: 8,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              fontWeight: "bold",
-              color: "#000000",
-            }}
-          >
-            {level}
-          </Text>
-        </LinearGradient>
+        <ButtonNoClick
+          colorsStart="#86B8F3"
+          colorsEnd="#2DC897"
+          padding={0}
+          radius={5}
+          height={26}
+          width={51}
+          fontSize={12}
+          fontWeight="bold"
+          fontcolor="#000"
+          text={level}
+        />
         <Text
           style={{
             fontSize: 16,
             fontWeight: "bold",
             color: "#000000",
+            marginLeft: "3%",
           }}
         >
           {name}
