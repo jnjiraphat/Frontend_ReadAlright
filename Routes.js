94a576de922462b33d7fa7771902a1603c325264
Jenny <jnjiraphat@gmail.com>
2020-04-21 23:42:29 +0700

    [ADD] Gradient CSS Navbar

--- Routes.js
+++ Routes.js
@@ -9,18 +9,12 @@ import MyHome from "./App/Screens/Home.js";
 // import Home from "../App/Screens/Home";
 import Tricks from "./App/Screens/Tricks";
 import Mylist from "./App/Screens/Mylist";
-import { Text } from "react-native";
 // import Article from "./App/Screens/Article"
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-  ImageBackground,
-  FlatList,
-} from "react-native";
+import { StyleSheet, Dimensions, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const TabIcon = ({ focused, title }) => {
   // var color = focused ? "black" : "grey";
@@ -29,42 +23,49 @@ const TabIcon = ({ focused, title }) => {
   if (title == "Home") {
     if (focused) {
       return (
-        <MaterialCommunityIcons
-          name="home-variant"
-          size={28}
-        ></MaterialCommunityIcons>
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <MaterialCommunityIcons name="home-variant" size={28} />
+        </LinearGradient>
       );
     } else {
       return (
-        <MaterialCommunityIcons
-          name="home-outline"
-          size={28}
-        ></MaterialCommunityIcons>
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <MaterialCommunityIcons name="home-outline" size={28} />
+        </LinearGradient>
       );
     }
   }
   if (title == "Trick") {
     if (focused) {
       return (
-        <MaterialCommunityIcons
-          name="lightbulb-on"
-          size={28}
-        ></MaterialCommunityIcons>
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <MaterialCommunityIcons name="lightbulb-on" size={28} />
+        </LinearGradient>
       );
     } else {
       return (
-        <MaterialCommunityIcons
-          name="lightbulb-on-outline"
-          size={28}
-        ></MaterialCommunityIcons>
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <MaterialCommunityIcons
+            name="lightbulb-on-outline"
+            size={28}
+          ></MaterialCommunityIcons>
+        </LinearGradient>
       );
     }
   }
   if (title == "Mylist") {
     if (focused) {
-      return <FontAwesome name="bookmark" size={28}></FontAwesome>;
+      return (
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <FontAwesome name="bookmark" size={28}></FontAwesome>
+        </LinearGradient>
+      );
     } else {
-      return <FontAwesome name="bookmark-o" size={28}></FontAwesome>;
+      return (
+        <LinearGradient colors={["#7EF192", "#2DC897"]} style={styles.gradient}>
+          <FontAwesome name="bookmark-o" size={28}></FontAwesome>
+        </LinearGradient>
+      );
     }
   }
   // return <Text>{selected}</Text>
@@ -86,7 +87,7 @@ const Routes = () => {
         <Scene
           key="tabbar"
           tabs
-          tabBarStyle={{ backgroundColor: "white", height: 61 }}
+          tabBarStyle={{ height: 61 }}
           wrap={false}
           showLabel={false}
         >
@@ -121,4 +122,11 @@ const Routes = () => {
 };
 export default Routes;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  gradient: {
+    width: Dimensions.get("window").width / 3,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
