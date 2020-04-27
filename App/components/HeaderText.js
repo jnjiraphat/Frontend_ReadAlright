11a285582b181f88f736d667d2e9fb07f483977a
Jenny <jnjiraphat@gmail.com>
2020-04-27 23:09:50 +0700

    [ADD] Component HeaderText

---
+++ App/components/HeaderText.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { useFonts } from "@use-expo/font";
+
+// function useFonts(fontMap) {
+//   let [fontsLoaded, setFontsLoaded] = useState(false);
+//   // (async () => {
+//   //   await Font.loadAsync(fontMap);
+//   //   setFontsLoaded(true);
+//   // })();
+//   return [fontsLoaded];
+// }
+
+const HeaderText = (props) => {
+  const { text } = props;
+  // let [fontsLoaded] = useFonts({
+  //   // "PTSansCaption-Regular": require("../assets/font/PTSansCaption-Regular.ttf"),
+  //   "PTSansCaption-Bold": require("../assets/font/PTSansCaption-Bold.ttf"),
+  // });
+
+  // if (!fontsLoaded) {
+  //   return console.log("Font not load");
+  // } else {
+  return <Text style={styles.headerText}>{text}</Text>;
+  // }
+};
+
+export default HeaderText;
+
+const styles = StyleSheet.create({
+  headerText: {
+    // fontFamily: "PTSansCaption-Bold",
+    fontSize: 24,
+  },
+});
