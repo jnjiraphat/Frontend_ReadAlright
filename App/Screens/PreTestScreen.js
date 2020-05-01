8153ab686992cfbc64e5012f0d9ebd4cdafb9654
Jenny <jnjiraphat@gmail.com>
2020-05-01 23:56:15 +0700

    [ADD] Modal

--- App/Screens/PreTestScreen.js
+++ App/Screens/PreTestScreen.js
@@ -8,6 +8,8 @@ import {
   StyleSheet,
   SectionList,
   SafeAreaView,
+  Modal,
+  TouchableHighlight,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Constants from "expo-constants";
@@ -30,6 +32,8 @@ const PreTest = () => {
   useEffect(() => {
     Quiz();
   }, []);
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   const sections = [
     {
@@ -122,55 +126,85 @@ const PreTest = () => {
   //     />
   //   );
   // };
-  if (quizs.length>0) {
-    console.log(quizs.length)
-    return (
-      <LinearGradient
-        colors={["#FFB382", "#F07590"]}
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          top: 0,
-          height: Dimensions.get("window").height,
-          width: Dimensions.get("window").width,
-        }}
-      >
-        <ScrollView style={styles.container}>
-          <View style={{ alignItems: "center" }}>
-            <View style={styles.textLayout}>
-              <HeaderText text="Pre-Test" />
-              <Text style={styles.subHeader}>
-                Fill the gaps with the correct word from the box.
-              </Text>
-            </View>
-            <TestBox quizs={quizs}/>
-            <ButtonClick
-              text="Submit"
-              fontSize={24}
-              fontWeight="Bold"
-              fontcolor="#000000"
-              height={39}
-              width={245}
-              radius={30}
-              padding={0}
-              marginBottom="10%"
-              // onPressAction={goToHome}
-              // shadowRadius={30}
-              colorsStart="#7EF192"
-              colorsEnd="#2DC897"
-            />
+
+  // if (quizs.length>0) {
+  //   console.log(quizs.length)
+
+  return (
+    <LinearGradient
+      colors={["#FFB382", "#F07590"]}
+      style={{
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        height: Dimensions.get("window").height,
+        width: Dimensions.get("window").width,
+      }}
+    >
+      <ScrollView style={styles.container}>
+        <View style={{ alignItems: "center" }}>
+          <View style={styles.textLayout}>
+            <HeaderText text="Pre-Test" />
+            <Text style={styles.subHeader}>
+              Fill the gaps with the correct word from the box.
+            </Text>
           </View>
-        </ScrollView>
-      </LinearGradient>
-    );
-  } else {
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    );
-  }
+          <TestBox quizs={quizs} />
+
+          <ButtonClick
+            text="Submit"
+            fontSize={24}
+            fontWeight="bold"
+            fontcolor="#000000"
+            height={39}
+            width={245}
+            radius={30}
+            padding={0}
+            marginBottom="10%"
+            onPressAction={() => {
+              setModalVisible(true);
+            }}
+            // shadowRadius={30}
+            colorsStart="#7EF192"
+            colorsEnd="#2DC897"
+          />
+        </View>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+
+                <TouchableHighlight
+                  style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
+  // } else {
+  //   return (
+  //     <View>
+  //       <Text>Loading</Text>
+  //     </View>
+  //   );
+  // }
 
   // }
 };
@@ -219,5 +253,43 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     width: Dimensions.get("window").width / 1.35,
+  },
+
+  // modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
